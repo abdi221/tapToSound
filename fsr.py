4bf46b36b77f50befc08e13df7a6102973f7bd54
abdi221 <abdifatahali22@gmail.com>
2025-11-09 17:02:40 +0100

    Small adjustments needed

--- fsr.py
+++ fsr.py
@@ -8,20 +8,20 @@ fsr_pin = 28
 fsr = ADC(Pin(fsr_pin))
 
 while True:
-    analog_reading = fsr.read_16() # read the raw analog values
+    analog_reading = fsr.read_u16() # read the raw analog values
     analog_reading = analog_reading // 64 # scale it down to 0-1023 range to match arduino
     print("Force sensor reading: ", analog_reading)
     
-    if analog_reading < 6553: # from 0 to 6552
+    if analog_reading < 500:
         print(" -> no pressure")
-    elif analog_reading < 13107: # from 6552 to 13106
+    elif analog_reading < 900:
         print(" -> light touch")
-    elif analog_reading < 32767: # from 13106 to 32766
-        print(" -> light squeeze")
-    elif analog_reading < 52428: # from 32766 to 52427
+    elif analog_reading < 1000:
         print(" -> medium touch")
     else:
         print(" -> BIG SQUEEZE")
+        
+    time.sleep(1)
         
     
     
